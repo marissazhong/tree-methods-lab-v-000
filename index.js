3c19900aced4b046558d8ed0244b1fe8a9c77550
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 12:02:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ function inOrder(currentNode) {
 
 function findOrAdd(rootNode, newNode) {
   let currentNode = rootNode;
+  let result = 0;
   if (currentNode.data == newNode.data) {
     //*test: console.log(currentNode.data,newNode.data);
-    true;
+    result = -1;
   } else if (currentNode.data > newNode.data) { // move left
     currentNode = rootNode.left;
     if (currentNode) {
@@ -29,6 +30,7 @@ function findOrAdd(rootNode, newNode) {
       rootNode.right = newNode;
     }
   }
+  return result = -1 ? true : rootNode;
 }
 
 // let rootNode = {data: 5, left: null, right: null};
