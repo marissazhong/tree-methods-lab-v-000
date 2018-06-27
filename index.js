93594adbe5a0bb256d6c0f7885b9ce579820f1ae
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 11:41:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,13 +9,24 @@ function inOrder(currentNode) {
   }
 }
 
-function findOrAdd(root_node, new_node) {
-  let currentNode = root_node.data;
+function findOrAdd(rootNode, newNode) {
+  let currentNode = rootNode;
   //edge case: found new_node at root
-  if (currentNode == new_node) {
+  if (currentNode.data == newNode.data) {
     return true;
+  } else if (currentNode.data > newNode.data) { // move left
+    currentNode = rootNode.left;
+    if (currentNode) {
+      findOrAdd(currentNode, newNode);
+    } else {
+      rootNode.left = newNode;
+    }
+  } else if (currentNode < newNode) { // move right
+    currentNode = rootNode.right;
+    if (currentNode) {
+      findOrAdd(currentNode, newNode);
+    } else {
+      rootNode.right = newNode;
+    }
   }
-
-  
-  
 }
